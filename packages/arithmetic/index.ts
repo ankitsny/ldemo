05ed13bb4c4b07310d99c261aa54fb7cc0435efd
Ankit Kumar <ankit_sony3@hotmail.com>
2022-04-20 22:16:47 +0530

    initial commit

---
+++ packages/arithmetic/index.ts
@@ -0,0 +1,7 @@
+import { add } from '@ankso/add';
+import { sub } from '@ankso/sub';
+import { multiply } from '@ankso/multiply'
+
+console.log("Add: ", add(1, 2));
+console.log("Subtract: ", sub(10, 2));
+console.log("Multiply: ", multiply(10, 2));
